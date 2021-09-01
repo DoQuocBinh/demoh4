fcbcf772a8cd90879420aef0efac96d97b2a5f54
Binh Do <binhdq@fe.edu.vn>
2021-09-01 18:47:50 +0700

    ok

--- app.js
+++ app.js
@@ -9,7 +9,16 @@ app.get('/',async (req,res)=>{
     var client = await MongoClient.connect(connectionStr)
     let dbo = client.db("GCH0805DB");
     let results = await dbo.collection("students").find({}).toArray();
-    res.render('index',{data:results})
+    // results.forEach(element => {
+    //     console.log(element.name.charAt(0))
+    //     element['first'] = element.name.charAt(0)
+    // });
+    //Tao  them cot 'first' co gia tri la ky tu dau tien cua cot 'name'
+    for(i=0;i<results.length;i++){
+        results[i].first = results[i].name.charAt(0);
+    }
+    //chi hien thi 2 phan tu dau tien
+    res.render('index',{data:results.slice(0,2)})
 })
 
 const PORT = process.env.PORT || 5000
